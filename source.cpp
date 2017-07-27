50fe752b3085f026aa9491540dc2bf947e6d1e07
rlfdldjaak22 <rlfdldjaak22@naver.com>
2017-07-28 01:25:14 +0900

    parser's start point
    Regex JJangJJang

---
+++ source.cpp
@@ -0,0 +1,34 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <regex>
+#include <string>
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	size_t filesize;
+	FILE* fp;
+	string fuck;
+	regex pattern("([a-zA-Z_가-힣][a-zA-Z_가-힣0-9]*)(?=((은|는|이|가|을|를|와|에|\\(\\w*\\))\\s))");
+	smatch m;
+	if (argc < 2)
+	{
+		return 0;
+	}
+	fp = fopen(argv[1], "r");
+	while (!feof(fp))
+	{
+		fuck += fgetc(fp);
+	}
+	if (regex_search(fuck, m, pattern))
+	{
+		for (auto& sm : m)
+		{
+			printf("%s", sm);
+		}
+	}
+
+	return 0;
+}
