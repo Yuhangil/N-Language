8f665376472d004522d2a5e88693152e6d957739
Jeong Gyu-Yong <jeong000307@icloud.com>
2017-07-28 02:22:06 +0900

    첫 걸음

--- source.cpp
+++ source.cpp
@@ -8,24 +8,22 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	size_t filesize;
 	FILE* fp;
-	string fuck;
+	string str;
 	regex pattern("([a-zA-Z_가-힣][a-zA-Z_가-힣0-9]*)(?=((은|는|이|가|을|를|와|에|\\(\\w*\\))\\s))");
-	smatch m;
+	smatch match;
 
-	if (argc < 2)
-	{
+	if (argc < 2)	{
 		return 0;
 	}
 	fp = fopen(argv[1], "r");
-	while (!feof(fp))
-	{
-		fuck += fgetc(fp);
+	while (!feof(fp))	{
+		str += fgetc(fp);
 	}
-	string result = regex_replace(fuck, pattern, string("sex"));
-
-	printf("%s", result);
+	while (regex_search(str, match, pattern)) {
+        cout << match[0] << "\n";
+		str = match.suffix().str();
+    }
 
 	return 0;
 }
