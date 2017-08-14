bb7e9cb070716475f9cb81a86f140cd6a4bc727b
rlfdldjaak22 <rlfdldjaak22@naver.com>
2017-08-15 00:01:52 +0900

    파서의 기본
    헤더좀 입력함

--- Parser/Data.h
+++ Parser/Data.h
@@ -9,21 +9,31 @@
 #include <iostream>
 #include <queue>
 #include <stack>
+#include <regex>
 
 using namespace std;
+
+enum token
+{
+	eof = -1,
+	def = -2,
+	id = -3,
+	number = -4,
+
+};
 
 class Dataset
 {
 private:
+	regex pattern[4];
+	wstring buffer;
 
 public:
 	Dataset();
-	void print();
-	void getTable();
-	void isSymbol();
-	void input();
-
-	virtual ~Dataset();
+	//void print();
+	//void getTable();
+	bool isId(wchar_t ch);
+	int input(FILE* fp);
 };
 
 
--- Parser/main.cpp
+++ Parser/main.cpp
@@ -2,7 +2,7 @@
 
 
 
-int main(void)
+int main(int argc, char* argv[])
 {
 
 }
