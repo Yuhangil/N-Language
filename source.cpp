dbf980c974b8a1faa75aec0bdb67d1620ba91c0b
Jeong Gyu-Yong <jeong000307@icloud.com>
2017-08-20 00:06:16 +0900

    amolang

--- source.cpp
+++ source.cpp
@@ -5,25 +5,34 @@
 
 using namespace std;
 
+vector<wstring> Buffer;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[])	{
 	FILE* fp;
-	string str;
-	regex pattern("([a-zA-Z_가-힣][a-zA-Z_가-힣0-9]*)(?=((은|는|이|가|을|를|와|에|\\(\\w*\\))\\s))");
-	smatch match;
+	wstring String;
+	size_t FileSize;
+	regex idPattern("([a-zA-Z_가-힣][a-zA-Z_가-힣0-9]*)(?=((은|는|이|가|을|를|와|에|\\(\\w*\\))\\s))");
+	regex bracePattern("[{}]");
+	regex bracketPattern("[()]");
+	regex NumberPattern("(정수|실수)");
+	regex WordingPattern("(은|는|이|가|을|를|와|에)");
+	regex OperatorPattern("(더한|뺀|곱한|나눈)");
+	smatch Match;
 
 	if (argc < 2)	{
 		return 0;
 	}
+	
 	fp = fopen(argv[1], "r");
-	while (!feof(fp))	{
-		str += fgetc(fp);
-	}
-	while (regex_search(str, match, pattern)) {
-        cout << match[0] << "\n";
-		str = match.suffix().str();
-    }
+	fseek(fp, 0L, SEEK_END);
+	FileSize = ftell(fp);
+	fgets(String, FileSize, fp);
+	fseek(fp, 0L, SEEK_SET);
+
+		while (regex_search(str, Match, idPattern)) {
+        	cout << Match[0] << "\n";
+			Buffer.push_back(Match.suffix().str());
+		}
 
 	return 0;
 }
