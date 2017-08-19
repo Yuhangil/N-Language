21b80d1096ce0f78789b18d4a9a5f71bcd6a26f6
rlfdldjaak22 <rlfdldjaak22@naver.com>
2017-08-20 00:50:12 +0900

    Parser개발
    token 하나로 읽고 다음 으로 예상되는 토큰을 읽어오는 식으로 BNF 문법의 장점을 활용하고자 함.
    다만, 이 경우 먼저 문법을 테이블화 시킬 필요가 있음.
    다음으로 읽은 토큰이 예상된 토큰에 포함되지 않으면 오류로 판정

--- Parser/Data.h
+++ Parser/Data.h
@@ -1,39 +1,31 @@
 #ifndef __DATA__
 #define __DATA__
 #include <cstdio>
-#include <cmath>
-#include <vector>
-#include <set>
-#include <cstring>
-#include <algorithm>
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <regex>
+#include <string>
+#include <locale>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 enum token
 {
-	eof = -1,
-	def = -2,
-	id = -3,
-	number = -4,
-
+	
 };
 
 class Dataset
 {
 private:
-	regex pattern[4];
+	regex pattern[10];
 	wstring buffer;
-
+	smatch match;
+	wstring String;
 public:
 	Dataset();
-	//void print();
-	//void getTable();
-	bool isId(wchar_t ch);
-	int input(FILE* fp);
+	void input(char* filename);
+	void solve(wstring Buffer);
 };
 
 
